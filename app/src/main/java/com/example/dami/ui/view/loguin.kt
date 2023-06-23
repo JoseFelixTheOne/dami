38f772d7ab2e01f4db90f16b8671c5f44b57f8c0
Jose Felix <torrestjfelix@gmail.com>
2023-06-23 16:52:22 -0500

    Uniendo el login de Arismendiz y agregando la inicialización de la bd para poder levantar la app

---
+++ app/src/main/java/com/example/dami/ui/view/loguin.kt
@@ -0,0 +1,56 @@
+package com.example.dami.ui.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+
+import android.widget.Toast
+import com.example.dami.databinding.ActivityLoguinBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+
+class loguin : AppCompatActivity() {
+    private lateinit var binding: ActivityLoguinBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoguinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+
+        val email = binding.email.text.toString()
+        val pass = binding.password.text.toString()
+
+        auth = FirebaseAuth.getInstance()
+        binding.ingresar.setOnClickListener {
+            val email = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+
+            if (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (pass.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(email, pass)
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                        }
+                } else {
+                    binding.password.setError("Empty fields are not allowed")
+                }
+            } else if (email.isEmpty()) {
+                binding.email.setError("Empty fields are not allowed")
+            } else {
+                binding.email.setError("Please enter correct email")
+            }
+        }
+
+        binding.rediriccionarregistro.setOnClickListener {
+            //startActivity(Intent(this, register::class.java))
+        }
+    }
+}
